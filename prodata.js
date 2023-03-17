07d6828b7b73d3911fb24eba8c7efbc8151876f1
Anshcodeify45 <anishpatnaik45@gmail.com>
2023-03-17 23:21:23 +0530

    product update

--- prodata.js
+++ prodata.js
@@ -1,16 +1,17 @@
 var data = JSON.parse(localStorage.getItem("bewakoof_single_product")) || {};
 
 
-displayItems();
 
-function displayItems(){
+      document.getElementById("main_img_1").src=data.image;
+      document.getElementById("muliple_img1").src=data.image_1;
+      document.getElementById("muliple_img2").src=data.image_2;
+      document.getElementById("muliple_img3").src=data.image_3;
+      document.getElementById("muliple_img4").src=data.image_4;
+      document.getElementById("product_name_main").innerText=data.name;
+      document.getElementById("product_new_price_main").innerText=data.mrp;
+      document.getElementById("product_original_price_main").innerText =data.price;
+      document.getElementById("product_discount_percentage_main").innerText=data.discount;
 
-      document.getElementById("main_img_1").value=data.image;
-      document.getElementById("muliple_img1").value=data.image_1;
-      document.getElementById("muliple_img2").value=data.image_2;
-      document.getElementById("muliple_img3").value=data.image_3;
-      document.getElementById("muliple_img4").value=data.image_4;
-}
 
 
 
